c353abc2c0ec0efe7719d19658650c2f73238591
yangty152 <35477773+yangty152@users.noreply.github.com>
2021-06-24 21:39:58 -0400

    add gauge sample

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -58,6 +58,32 @@ d3.json("samples.json").then((data) => {
 
 Plotly.newPlot('bubble', data, layout);
 
+var data = [
+    {
+      domain: { x: [0, 1], y: [0, 1] },
+      value: 450,
+      title: { text: "Belly Button Washing Frequency" },
+      type: "indicator",
+      mode: "gauge+number+delta",
+      delta: { reference: 380 },
+      gauge: {
+        axis: { range: [null, 500] },
+        steps: [
+          { range: [0, 250], color: "lightgray" },
+          { range: [250, 400], color: "gray" }
+        ],
+        threshold: {
+          line: { color: "red", width: 4 },
+          thickness: 0.75,
+          value: 490
+        }
+      }
+    }
+  ];
+  
+  var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+  Plotly.newPlot('gauge', data, layout);
+
    // samples = data.samples;
 
 sample_metadata = metadata[0];
@@ -65,10 +91,10 @@ var metadata_arr = d3.entries(sample_metadata);
 d3.select("#sample-metadata").selectAll("div")
         .data(metadata_arr)
         .enter()
-        .append("div")
+        .append("p")
         .html(function(d){
             console.log(d);
-            return `${d["key"]}:${d["value"]}`;
+            return `<b>${d["key"]}:${d["value"]}</b>`;
     });
 console.log(metadata_arr);
 });    
