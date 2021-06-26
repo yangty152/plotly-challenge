1af279fd4e11f808c72e76dd400be2304b161dec
yangty152 <35477773+yangty152@users.noreply.github.com>
2021-06-26 14:09:12 -0400

    dynamically passed in id

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,21 +1,52 @@
 d3.json("samples.json").then((data) => {
-    //  Create the Traces
-    // filter the data set by input id, return the data set for the particular id
-    var names = data.names; //need to assign this array to the options
-    var metadata = data.metadata;
-    console.log(metadata);
-    var id = 940
+// populate the Test Subject ID No. drop down menu
+names = data.names; //need to assign this array to the options
+metadata = data.metadata;
+samples = data.samples;
+d3.select("#selDataset").selectAll("option")
+        .data(names)
+        .enter()
+        .append("option")
+        .html(function(d){
+            return `${d}`;
+    });
+});
 
+//define optionChanged function  
+function optionChanged(id){
+  console.log(id);
+  //d3.json("samples.json").then((data) => {  
+  //console.log(metadata);
+  //filter metadata for the id
+  var idint = parseInt(id);
+  console.log(metadata);
+  var sample_metadata = metadata.filter(metadata => metadata.id === idint);
+  console.log(sample_metadata);
+  var metadata_obj = sample_metadata[0]; //maybe make this more dynamic
+  var metadata_arr = d3.entries(metadata_obj);
+  console.log(metadata_arr);
+  d3.select("#sample-metadata").selectAll("div")
+        .data(metadata_arr)
+        .enter()
+        .append("p")
+        .html(function(d){
+            console.log(d);
+            return `<b>${d["key"]}:${d["value"]}</b>`;
+    });
     var testData = {"id": "940", "otu_ids": [1167, 2859, 482, 2264, 41, 1189, 352, 189, 2318, 1977, 3450, 874, 1959, 2191, 1950, 2077, 2275, 944, 2184, 2244, 2024, 2419, 2811, 165, 2782, 2247, 2011, 2396, 830, 2964, 1795, 2722, 307, 2178, 2908, 1193, 2167, 1208, 2039, 1274, 2739, 2737, 1314, 1962, 2186, 2335, 2936, 907, 833, 2483, 2475, 2491, 2291, 159, 2571, 2350, 2342, 2546, 725, 170, 1505, 513, 259, 1169, 258, 1232, 1497, 1498, 1503, 412, 2235, 1960, 1968, 121, 2065, 340, 2110, 2188, 357, 342], "sample_values": [163, 126, 113, 78, 71, 51, 50, 47, 40, 40, 37, 36, 30, 28, 25, 23, 22, 19, 19, 14, 13, 13, 13, 12, 12, 11, 11, 11, 10, 10, 10, 8, 7, 7, 7, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], "otu_labels": ["Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Proteobacteria;Epsilonproteobacteria;Campylobacterales;Campylobacteraceae;Campylobacter", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Actinomycetaceae;Varibaculum", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae;Corynebacterium", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Peptoniphilus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales", "Bacteria;Firmicutes;Clostridia;Clostridiales;Veillonellaceae", "Bacteria;Firmicutes;Bacilli;Bacillales;Staphylococcaceae;Staphylococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Finegoldia", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;Peptococcaceae;Peptococcus", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Prevotellaceae", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Gallicola", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Gallicola", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Prevotellaceae;Prevotella", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;Ruminococcaceae", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Corynebacteriaceae", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales;Actinomycetaceae", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI;Anaerococcus", "Bacteria;Actinobacteria;Actinobacteria;Actinomycetales", "Bacteria", "Bacteria;Firmicutes", "Bacteria", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria", "Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Porphyromonadaceae;Porphyromonas", "Bacteria;Firmicutes", "Bacteria;Firmicutes", "Bacteria;Firmicutes", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria;Firmicutes;Clostridia", "Bacteria;Firmicutes;Clostridia", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria", "Bacteria;Firmicutes;Clostridia;Clostridiales", "Bacteria;Firmicutes;Clostridia;Clostridiales;IncertaeSedisXI", "Bacteria", "Bacteria"]};
     //filter function to get metadata for selected id
-
+    var sample_data = samples.filter(samples => samples.id === id);
     // Identify the top 10 samples values
-    var otu_ids = testData.otu_ids;
-    var sample_values = testData.sample_values;
-    var sample_labels = testData.otu_labels;
+    //var otu_ids = sample_data.otu_ids;
+    var otu_ids = sample_data.map(function(d) { return d.otu_ids; })
+    //var sample_values = sample_data.sample_values;
+    var sample_values = sample_data.map(function(d) { return d.sample_values; })
+    //var sample_labels = sample_data.otu_labels;
+    var sample_labels = sample_data.map(function(d) { return d.sample_labels; })
 
-    top_10_sample_values = sample_values.slice(0, 10);
-    top_10_otu_ids = otu_ids.slice(0,10);
+    top_10_sample_values = sample_values[0].slice(0, 10);
+    top_10_otu_ids = otu_ids[0].slice(0,10);
+    console.log(top_10_otu_ids);
     top_10_sample_values = top_10_sample_values.reverse()
     top_10_otu_ids = top_10_otu_ids.reverse()
     var otu_ids_string = top_10_otu_ids.toString();
@@ -39,14 +70,14 @@ d3.json("samples.json").then((data) => {
       Plotly.newPlot("bar", test, layout);
     
     var trace2 = {
-        x: sample_values,
-        y: otu_ids,
+        x: sample_values[0],
+        y: otu_ids[0],
         mode: 'markers',
         marker: {
-            size: sample_values,
-            color: otu_ids,
+            size: sample_values[0],
+            color: otu_ids[0],
         },
-        text: sample_labels,
+        text: sample_labels[0],
     };
 
     var data = [trace2];
@@ -86,15 +117,5 @@ var data = [
 
    // samples = data.samples;
 
-sample_metadata = metadata[0];
-var metadata_arr = d3.entries(sample_metadata);
-d3.select("#sample-metadata").selectAll("div")
-        .data(metadata_arr)
-        .enter()
-        .append("p")
-        .html(function(d){
-            console.log(d);
-            return `<b>${d["key"]}:${d["value"]}</b>`;
-    });
-console.log(metadata_arr);
-});    
+//});
+};
